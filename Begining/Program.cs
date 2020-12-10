88affeac19246c7a056153b5cd173a7a38a2a33a
estepanova <stepanova_eg@bk.ru>
2020-12-10 15:04:47 +0300

    Add project files.

---
+++ Begining/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Begining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Как Ваше имя? ");
+
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now}!");
+            Console.ReadLine();
+
+        }
+    }
+}
