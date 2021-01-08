d5dc36325bb1776d6a660d02a4a8e6c2228bb38f
estepanova <stepanova_eg@bk.ru>
2021-01-08 21:43:01 +0300

    HW to lesson 8

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (string.IsNullOrEmpty(Properties.Settings.Default.UserName))
+            {
+                Console.WriteLine("Enter your name:");
+                Properties.Settings.Default.UserName = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+
+            if (string.IsNullOrEmpty(Properties.Settings.Default.Profession))
+            {
+                Console.WriteLine("Enter your proffesion:");
+                Properties.Settings.Default.Profession = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+
+            string userName = Properties.Settings.Default.UserName;
+            string greeting = Properties.Settings.Default.Greeting;
+            string profession = Properties.Settings.Default.Profession;
+            Console.WriteLine($"{greeting}, {userName}, {profession}!");
+            Console.ReadLine();
+        }
+    }
+}
