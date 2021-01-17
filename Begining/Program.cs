8d4f61b8e5ae1047a71eb622fea11f4ccc5bf4bf
estepanova <stepanova_eg@bk.ru>
2021-01-17 21:30:42 +0300

    HW to  Lesson 1 Algorithms

--- Begining/Program.cs
+++ Begining/Program.cs
@@ -2,17 +2,62 @@
 
 namespace Begining
 {
+    /*
+     Требуется реализовать на C# функцию согласно блок-схеме. Блок-схема описывает алгоритм проверки, простое число или нет.
+     Написать консольное приложение.
+     Алгоритм реализовать отдельно в функции согласно блок-схеме.
+     Написать проверочный код в main функции .
+     Код выложить на GitHub.
+     */
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Как Ваше имя? ");
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+           
+            Console.Write("Введите число:  ");
 
-            var name = Console.ReadLine();
+            bool isNumber = (int.TryParse(Console.ReadLine(), out int number));
+            
+            if (isNumber)
+            {
+                Console.Write(DetermineNumber(number));
+            }
+            else
+            {
+                Console.Write($"Введенное значение имеет неверный формат!");
+            }
+        }
 
-            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now}!");
-            Console.ReadLine();
+        private static string DetermineNumber(int number)
+        {
+            int d = 0;
+            int i = 2;
+            string result;
 
+            while (i < number)
+            {
+                if (number % i == 0)
+                {
+                    d++;
+                }
+
+                i++;
+            }
+
+            if (d == 0)
+            {
+                result = "Число просто";
+            }
+            else
+            {
+                result ="Число не простое";
+            }
+
+            return result;
         }
     }
 }
---
+++ Task_1_2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1_2
+{
+    /*Вычислите асимптотическую сложность функции из примера ниже. */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int StrangeSum(int[] inputArray)
+        {
+            int sum = 0;
+            for (int i = 0; i < inputArray.Length; i++) // O(N)
+            {
+                for (int j = 0; j < inputArray.Length; j++) // O(N)
+                {
+
+                    for (int k = 0; k < inputArray.Length; k++) // O(N)
+                    {
+                        int y = 0; // O(1)
+
+                        if (j != 0) // O(1)
+                        {
+                            y = k / j; // O(1)
+                        }
+
+                        sum += inputArray[i] + i + k + j + y; // O(1)
+                    }
+                }
+            }
+
+            // Итого  O(N × N x N) = O(N^3).
+
+            return sum;
+        }
+    }
+}
