e30aedd78048944809b4b5fed0bd008c264cc188
estepanova <stepanova_eg@bk.ru>
2021-02-07 16:19:09 +0300

    HW lesson 5 Algorithms

---
+++ Begining/BinaryTree.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Begining
+{
+    public class BinaryTree
+    {
+        public Node Root
+        {
+            get;
+            internal set;
+        }
+
+        public void Add(int val)
+        {
+
+            if (Root == null)
+            {
+                Root = new Node(val, null, this);
+            }
+            else
+            {
+                InsertNode(Root, val);
+            }
+        }
+
+        public void Delete(int val)
+        {
+            if (Root == null)
+            {
+                return;
+            }
+
+            var node = Search(val);
+
+            if (node != null)
+            {
+                DeleteNode(node);
+            }
+            else
+            {
+                throw new Exception("No such element");
+            }
+        }
+
+        public Node Search(int val)
+        {
+            return SearchNode(val, Root);
+        }
+
+        private void InsertNode(Node subtree, int val)
+        {
+
+            if (val < subtree.Data)
+            {
+                if (subtree.Left == null)
+                {
+                    subtree.Left = new Node(val, subtree, this);
+                }
+                else
+                {
+                    InsertNode(subtree.Left, val);
+                }
+            }
+            else if (subtree.Right == null)
+            {
+                subtree.Right = new Node(val, subtree, this);
+            }
+            else
+            {
+                InsertNode(subtree.Right, val);
+            }
+
+            subtree.Balance();
+        }
+
+        private Node SearchNode(int val, Node subtree)
+        {
+            if (subtree == null) return null;
+
+            switch (val.CompareTo(subtree.Data))
+            {
+                case 1: return SearchNode(val, subtree.Right);
+                case -1: return SearchNode(val, subtree.Left);
+                case 0: return subtree;
+                default: return null;
+            }
+        }
+
+        public Node BFSSearch(int val)
+        {
+            var queue = new Queue<Node>();
+            Node searchedNode = null;
+            queue.Enqueue(Root);
+            Console.WriteLine($"Root - {Root.Data}");
+            while (queue.Count > 0)
+            {
+                Node node = queue.Dequeue();
+                Console.WriteLine($"Dequeued node - {node.Data}");
+
+                if (node.Data == val)
+                {
+                    searchedNode = node;
+                    Console.WriteLine($"Searched node - {node.Data}");
+                    break;
+                }
+
+                if (node.Left != null)
+                {                   
+                    queue.Enqueue(node.Left);
+                }
+                if (node.Right != null)
+                {
+                    queue.Enqueue(node.Right);
+                }
+            }
+
+            return searchedNode;
+        }
+
+        public Node DFSSearch(int val)
+        {
+            var stack = new Stack<Node>();
+            Node searchedNode = null;
+            stack.Push(Root);
+            Console.WriteLine($"Root - {Root.Data}");
+
+            while (stack.Count > 0)
+            {
+                Node node = stack.Pop();
+                Console.WriteLine($"Poped node - {node.Data}");
+
+                if (node.Data == val)
+                {
+                    searchedNode = node;
+                    Console.WriteLine($"Searched node - {node.Data}");
+                    break;
+                }
+
+                if (node.Left != null)
+                {
+                    stack.Push(node.Left);
+                }
+                if (node.Right != null)
+                {
+                    stack.Push(node.Right);
+                }
+            }
+
+            return searchedNode;
+        }
+
+        private bool DeleteNode(Node node)
+        {
+            Node parentNode = node.Parent;
+
+            if (node.Right == null)
+            {
+                if (node.Parent == null)
+                {
+                    Root = node.Left;
+
+                    if (Root != null)
+                    {
+                        Root.Parent = null;
+                    }
+                }
+                    else
+                    {
+                        int result = node.Parent.Data.CompareTo(node.Data);
+
+                        if (result > 0)
+                        {
+                            node.Parent.Left = node.Left;
+                        }
+                        else if (result < 0)
+                        {
+                            node.Parent.Right = node.Left;
+                        }
+                    }
+            }
+            else if (node.Right.Left == null)
+            {
+                node.Right.Left = node.Left;
+
+                if (node.Parent == null)
+                {
+                    Root = node.Right;
+
+                    if (Root != null)
+                    {
+                        Root.Parent = null;
+                    }
+                }
+                else
+                {
+                    int result = node.Parent.Data.CompareTo(node.Data);
+                    if (result > 0)
+                    {
+                        node.Parent.Left = node.Right;
+                    }
+                    else if (result < 0)
+                    {
+                        node.Parent.Right = node.Right;
+                    }
+                }
+            }
+            else
+            {
+                Node leftmost = node.Right.Left;
+
+                while (leftmost.Left != null)
+                {
+                    leftmost = leftmost.Left;
+                }
+                leftmost.Parent.Left = leftmost.Right;
+                leftmost.Left = node.Left;
+                leftmost.Right = node.Right;
+
+                if (node.Parent == null)
+                {
+                    Root = leftmost;
+
+                    if (Root != null)
+                    {
+                        Root.Parent = null;
+                    }
+                }
+                else
+                {
+                    int result = node.Parent.Data.CompareTo(node.Data);
+
+                    if (result > 0)
+                    {
+                        node.Parent.Left = leftmost;
+                    }
+                    else if (result < 0)
+                    {
+                        node.Parent.Right = leftmost;
+                    }
+                }
+            }
+
+            if (parentNode != null)
+            {
+                parentNode.Balance();
+            }
+
+            else
+            {
+                if (Root != null)
+                {
+                    Root.Balance();
+                }
+            }
+
+            return true;
+        }
+
+        public void Print()
+        {
+            Print(Root, 4);
+        }
+        public void Print(Node node, int padding)
+        {
+            if (node != null)
+            {
+                if (node.Right != null)
+                {
+                    Print(node.Right, padding + 4);
+                }
+
+                if (padding > 0)
+                {
+                    Console.Write(" ".PadLeft(padding));
+                }
+
+                if (node.Right != null)
+                {
+                    Console.Write("/\n");
+                    Console.Write(" ".PadLeft(padding));
+                }
+
+                Console.Write("(" + node.Data.ToString() + ")" + "\n ");
+                if (node.Left != null)
+                {
+                    Console.Write(" ".PadLeft(padding) + "\\\n");
+                    Print(node.Left, padding + 4);
+                }
+            }
+        }
+    }
+}
+
--- Begining/Program.cs
+++ Begining/Program.cs
@@ -2,17 +2,30 @@
 
 namespace Begining
 {
+    /*Реализуйте DFS и BFS для дерева с выводом каждого шага в консоль. */
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Как Ваше имя? ");
+            BinaryTree btr = new BinaryTree();
+            btr.Add(6);
+            btr.Add(2);
+            btr.Add(3);
+            btr.Add(35);
+            btr.Add(16);
+            btr.Add(21);
+            btr.Print();
 
-            var name = Console.ReadLine();
+            Console.WriteLine($"BFS Search - 6");
+            btr.BFSSearch(6);
+            Console.WriteLine($"BFS Search - 21");
+            btr.BFSSearch(21);
 
-            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now}!");
-            Console.ReadLine();
+            Console.WriteLine($"DFS Search - 6");
+            btr.DFSSearch(6);
 
+            Console.WriteLine($"DFS Search - 21");
+            btr.DFSSearch(21);
         }
     }
 }
