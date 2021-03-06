df9f6dd316b65b14648b817f891cb59a74141612
estepanova <stepanova_eg@bk.ru>
2021-03-06 11:46:17 +0300

    HW to lesson 8 Algorithms

--- Begining/Program.cs
+++ Begining/Program.cs
@@ -1,18 +1,98 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Begining
 {
+    /// <summary>
+    /// 1. Bucketsort Реализовать Bucketsort, проверить корректность работы.
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Как Ваше имя? ");
+            int[] integers = new int[15];
+            Random rnd = new Random();
 
-            var name = Console.ReadLine();
+            for (int i = 0; i < integers.Length; ++i)
+                integers[i] = rnd.Next(-99, 100);
 
-            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now}!");
-            Console.ReadLine();
+            Console.WriteLine(String.Join(" ", integers));
+            BucketSort(integers);
+            Console.WriteLine(String.Join(" ", integers));
 
+        }
+
+        private static void BucketSort(int[] integers)
+        {            
+            if (integers == null || integers.Length == 0)
+                return;
+            
+            int maxValue = integers[0]; 
+            int minValue = integers[0];
+            
+            for (int i = 1; i < integers.Length; i++)
+            {
+                if (integers[i] > maxValue)
+                    maxValue = integers[i];
+
+                if (integers[i] < minValue)
+                    minValue = integers[i];
+            }
+
+            List<int>[] buckets = new List<int>[integers.Length];
+            int bucketLength = 10;
+            int numRange = maxValue - minValue;
+
+            for (int i = 0; i < buckets.Length; i++)
+            {
+                buckets[i] = new List<int>();
+            }            
+
+            for (int i = 0; i < integers.Length; ++i) {
+                
+                int bucketIdx = (int)Math.Floor((double)(integers[i] - minValue) / numRange * bucketLength);
+                buckets[bucketIdx].Add(integers[i]);
+            }
+            
+            for (int i = 0; i < buckets.Length; ++i)
+            {
+                InsertionSort(buckets[i]);
+            }
+                
+            //Move items in the bucket back to the original array in order
+            int k = 0; 
+            for (int i = 0; i < buckets.Length; i++)
+            {
+                if (buckets[i].Count > 0)
+                {
+                    for (int j = 0; j < buckets[i].Count; j++)
+                    {
+                        integers[k] = buckets[i][j];
+                        k++;
+                    }
+                }
+            }
+        }
+
+        public static List<int> InsertionSort(List<int> bucket)
+        {
+            for (int i = 1; i < bucket.Count; i++)
+            {                
+                int currentValue = bucket[i];
+                int pointer = i - 1;
+                
+                while (pointer >= 0)
+                {                    
+                    if (currentValue < bucket[pointer])
+                    {                        
+                        bucket[pointer + 1] = bucket[pointer];
+                        bucket[pointer] = currentValue;
+                    }
+                    else break;
+                }
+            }
+
+            return bucket;
         }
     }
 }
---
+++ Task_8_2_Algorithms/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Task_8_2_Algorithms
+{
+    /// <summary>
+    /// Дописать реализацию Bucketsort до возможности сортировки больших массивов из файла (External sort).
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Console.ReadLine();
+            CreateBuckets(filePath);
+            BucketSort();
+        }
+
+        static void CreateBuckets(string file)
+        {
+            int bucket_num = 1;
+
+            StreamWriter sw = new StreamWriter(string.Format("C:\\bucket{0:d5}.dat", bucket_num));
+
+            using (StreamReader sr = new StreamReader(file))
+            {
+                while (sr.Peek() >= 0)
+                {                   
+                    sw.WriteLine(sr.ReadLine());
+
+                    if (sw.BaseStream.Length > 1000 && sr.Peek() >= 0)
+                    {
+                        sw.Close();
+                        bucket_num++;
+                        sw = new StreamWriter(string.Format("C:\\bucket{0:d5}.dat", bucket_num));
+                    }
+                }
+            }
+
+            sw.Close();
+        }
+        static void BucketSort()
+        {
+            foreach (string path in Directory.GetFiles("C:\\", "bucket*.dat"))
+            {                
+                string[] bucketCont = File.ReadAllLines(path);
+                
+                Array.Sort(bucketCont);
+                
+                string newpath = path.Replace("bucket", "sorted");
+               
+                File.WriteAllLines(newpath, bucketCont);
+               
+                File.Delete(path);
+                
+                bucketCont = null;
+
+                GC.Collect();
+            }
+        }
+
+        static void Merge()
+        {
+            string[] paths = Directory.GetFiles("C:\\", "sorted*.dat");
+            int bucketsNumber = paths.Length;
+            int records = 1000;
+           
+            StreamReader[] readers = new StreamReader[bucketsNumber];
+            for (int i = 0; i < bucketsNumber; i++)
+                readers[i] = new StreamReader(paths[i]);
+
+            StreamWriter sw = new StreamWriter("C:\\MergeSortedFile.txt");
+                        
+            Queue<string>[] queues = new Queue<string>[bucketsNumber];
+            for (int i = 0; i < bucketsNumber; i++)
+                queues[i] = new Queue<string>(records);
+                        
+            for (int i = 0; i < bucketsNumber; i++)
+            {
+                for (int k = 0; k < records; k++)
+                {
+                    if (readers[i].Peek() < 0) break;
+                    queue.Enqueue(readers[i].ReadLine());
+                }
+            }           
+            
+            bool done = false;
+            int lowestIndex;
+            string lowestValue;
+
+            while (!done)
+            {   
+                lowest_index = -1;
+                lowest_value = "";
+
+                for (j = 0; j < bucketsNumber; j++)
+                {
+                    if (queues[j] != null)
+                    {
+                        if (lowest_index < 0 || String.CompareOrdinal(queues[j].Peek(), lowest_value) < 0)
+                        {
+                            lowest_index = j;
+                            lowest_value = queues[j].Peek();
+                        }
+                    }
+                }
+                
+                if (lowest_index == -1) { 
+                    done = true; 
+                    break; 
+                }
+                               
+                sw.WriteLine(lowest_value);
+                
+                queues[lowest_index].Dequeue();                
+            }
+
+            sw.Close();
+            
+            for (int i = 0; i < bucketsNumber; i++)
+            {
+                readers[i].Close();
+                File.Delete(paths[i]);
+            }
+        }
+    }
+}
